fe4b29614887ff93375a9f3838e506ee028e5523
bin <tuduweb@qq.com>
2022-03-26 23:49:26 +0800

    完善“我的”页面

--- pages/me/index.js
+++ pages/me/index.js
@@ -7,23 +7,28 @@ Page({
   data: {
 		items: [
 			{
-				icon: '../../assets/images/iconfont-order.png',
-				text: '我的订单',
+				icon: '../../assets/images/more_icon1@2x.png',
+				text: '学习记录',
 				path: '/pages/order/list/index'
 			}, 
 			{
-				icon: '../../assets/images/iconfont-addr.png',
-				text: '收货地址',
+				icon: '../../assets/images/more_icon2@2x.png',
+				text: '笔记本',
 				path: '/pages/address/list/index'
 			}, 
 			{
-				icon: '../../assets/images/iconfont-kefu.png',
-				text: '联系客服',
+				icon: '../../assets/images/more_icon3@2x.png',
+				text: '我的分享',
 				path: '18521708248',
 			}, 
 			{
-				icon: '../../assets/images/iconfont-help.png',
-				text: '常见问题',
+				icon: '../../assets/images/more_icon4@2x.png',
+				text: '邀请好友',
+				path: '/pages/help/list/index',
+      },
+      {
+				icon: '../../assets/images/more_icon5@2x.png',
+				text: '帮助和反馈',
 				path: '/pages/help/list/index',
 			},
 		]
